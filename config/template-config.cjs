b20e8f5911640dea54653224176482501b548d76
Yuqi Guo <74295666+Eliteguo@users.noreply.github.com>
2023-10-21 16:36:42 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -6,7 +6,7 @@ const TEMPLATE_CONFIG = [
     desc: `
      🗓️{{date.DATA}}
 
-    今天是我们在一起的第{{love_day.DATA}}天，爱你❤️
+    今天是我们相识的第{{love_day.DATA}}天，爱你❤️
 
     今日{{city.DATA}}天气☁️：{{tian_api_weather_weather_0.DATA}}
     温度🌡️: {{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
